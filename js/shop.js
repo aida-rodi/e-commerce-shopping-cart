e6350adfc7ffb2278ec4ea3e06240273a5ed4e81
Aida Rodríguez <aida.rodi.1997@gmail.com>
2023-03-30 12:00:02 +0200

    exercise 1

--- js/shop.js
+++ js/shop.js
@@ -73,8 +73,14 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i]
+        if (id === product.id) {
+            cartList.push(product)
+            console.log(cartList)
+            return
+        }
+    }
 }
 
 // Exercise 2
